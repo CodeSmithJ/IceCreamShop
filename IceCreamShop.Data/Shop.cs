9f1062e26026a17883931279b8e099f6bc1f94c9
Jesse Smith <worldjesses@gmail.com>
2021-06-29 13:50:43 -0400

    Shop Data Entered / Web.config

---
+++ IceCreamShop.Data/Shop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceCreamShop.Data
+{
+    public class Shop
+    {
+        [Key]
+        public int ShopId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string IceCreamOrderId { get; set; }
+        [Required]
+        public double TotalPrice { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
