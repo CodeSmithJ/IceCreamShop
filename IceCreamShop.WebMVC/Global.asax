0b95a41ee49db0316799d300660dd07372d6517f
Jesse Smith <worldjesses@gmail.com>
2021-06-29 11:40:35 -0400

    Initial Commit

---
+++ IceCreamShop.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IceCreamShop.WebMVC.MvcApplication" Language="C#" %>
---
+++ IceCreamShop.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IceCreamShop.WebMVC.Startup))]
+namespace IceCreamShop.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
